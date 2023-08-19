cb07fd335b6ba1a0d3f4b489c294abbe64d4447d
jackdaw114 <80084582+jackdaw114@users.noreply.github.com>
2023-08-19 10:21:13 -0400

    im petty

--- frontend/my-app/src/components/Home2.js
+++ frontend/my-app/src/components/Home2.js
@@ -179,7 +179,7 @@ export default function Home() {
             <Toolbar  >
                 <AppBar sx={{ backgroundColor: '#292F36', justifyContent: 'center', alignItems: 'center', borderBottom: '20px solid #136F63' }} elevation={0}>
                     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 2 }}>
-                        <img src={img} style={{ height: 100 }} />
+                        <img src={img} style={{ height: 70 }} />
                         <Typography variant="h3" sx={{
                             textShadow: '1px 1px 1px rgba(255,255,255,0.9)', color: 'transparent', backgroundClip: 'text',
                             backgroundColor: '#333333', fontFamily: 'Anton', padding: 1
