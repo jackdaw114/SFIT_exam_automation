48fb04c486504258c20a92615895a34d4e220123
Nigel <95841502+LearnerOfProgramming@users.noreply.github.com>
2023-08-20 21:01:00 +0530

    Update VerifyEligibility.js

--- frontend/my-app/src/components/VerifyEligibility.js
+++ frontend/my-app/src/components/VerifyEligibility.js
@@ -4,11 +4,17 @@ import Header from './Header';
 import NewSheet from './NewSheet';
 
 export default function VerifyEligibility() {
+    const DummyData = [
+        { id: 1, name: 'Jason', age: 21, class: 'TEACMPN' ,hobby: 'coding' },
+        { id: 2, name: 'Aslin', age: 17, class: 'TEACMPN' ,hobby: 'basketball' },
+        { id: 3, name: 'Nigel', age: 20, class: 'TEACMPN' ,hobby: 'valorant' },
+        { id: 4, name: 'Callahan', age: 20, class: 'TEACMPN' ,hobby: 'Women' },
+      ]
     return (
         <>
             <Box className='h_background' sx={{ flexGrow: 1, minHeight: '100vh' }}>
                 <Header />
-                <NewSheet/>
+                <NewSheet table={DummyData } />
             </Box>
         </>
     )
