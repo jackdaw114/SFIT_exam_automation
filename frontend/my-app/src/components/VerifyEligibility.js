0e45c704685bfd5559e3dee4e51f1d71c8f5233a
Nigel <95841502+LearnerOfProgramming@users.noreply.github.com>
2023-08-20 20:58:30 +0530

    Blah

--- frontend/my-app/src/components/VerifyEligibility.js
+++ frontend/my-app/src/components/VerifyEligibility.js
@@ -1,12 +1,14 @@
 
 import { Typography,Box } from '@mui/material';
 import Header from './Header';
+import NewSheet from './NewSheet';
 
 export default function VerifyEligibility() {
     return (
         <>
             <Box className='h_background' sx={{ flexGrow: 1, minHeight: '100vh' }}>
-                <Header/>
+                <Header />
+                <NewSheet/>
             </Box>
         </>
     )
