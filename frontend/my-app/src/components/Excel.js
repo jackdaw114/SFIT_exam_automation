da5e89cf032c806a35af618613349406fcf24713
Nigel <95841502+LearnerOfProgramming@users.noreply.github.com>
2023-08-28 13:50:53 +0530

    working excel meow

--- frontend/my-app/src/components/Excel.js
+++ frontend/my-app/src/components/Excel.js
@@ -86,3 +86,21 @@ export default function Excel() {
     </div>
     )
 }
+
+
+
+export function toExcel(jsonData) {
+    const sheet = utils.json_to_sheet(jsonData, { header: Object.keys(jsonData[0]) })
+    console.log(sheet)
+    const wb = utils.book_new()
+    utils.book_append_sheet(wb, sheet, 'sheet1')
+   // writeFile(wb,'test.xlsx')
+    const binaryString = write(wb, {
+        bookType: 'xlsx',
+        bookSST: false,
+        type: 'binary',
+    });
+    const test = read(binaryString)
+    console.log(test)
+    return binaryString
+}
