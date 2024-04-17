b56c05147a882063b7555b09d867e5d973f008c8
jackdaw <jasonsampypvt@gmail.com/home/jackdaw/Documents/GitHub/opengl_windowsXlinux/build/bin/GL_project>
2024-04-17 16:59:41 +0530

    backend

--- backend/jason.js
+++ backend/jason.js
@@ -54,7 +54,7 @@ router.post('/create_student_marks', async (req, res) => {
         })
 
         const check_teacher = await TeacherSubjectsSchema.findOne({ teacher_id: req.body.teacher_id })
-        let flag = false;
+        let flag = true;
         console.log(check_teacher.created[req.body.marks_type])
         if (!check_teacher.created[req.body.marks_type]) {
             flag = true;
@@ -145,6 +145,177 @@ router.post('/get_exam', async (req, res) => {
         res.status(400).send(err.keyValue)
     }
 })
+
+router.post('/update_subject_list', async (req, res) => {
+    try {
+        let existingSubjectList = await SubjectListSchema.findOne({ semester: req.body.semester, branch: req.body.branch })
+        if (existingSubjectList) {
+            existingSubjectList.subject_ids = req.body.subject_ids;
+            subjectList = await existingSubjectList.save();
+        }
+        else {
+            SubjectList = new SubjectListSchema(req.body);
+            await SubjectList.save();
+        }
+        res.send('updated')
+    } catch (err) {
+        console.log(err)
+        res.status(400).send(err.keyValue)
+    }
+})
+
+router.post('/create_gazette', async (req, res) => {
+    try {
+        let students = await StudentSchema.find({ semester: req.body.semester, branch: req.body.branch },
+            // {
+            //     pid: true,
+            //     name: true,
+            //     oral: true,
+            //     practical: true,
+            //     term: true,
+            // }
+        )
+        let SubjectList = await SubjectListSchema.findOne({ semester: req.body.semester, branch: req.body.branch }, {
+            subject_ids: true
+        }) // TODO: this might not be useful but go figure
+
+        SubList = SubjectList.subject_ids
+        console.log(students)
+        console.log(SubjectList)
+        console.log("hello")
+        const template = xlsx.readFile('./ExcelTemplates/gazette_temp.xlsx')
+        const temp_sheet = template.Sheets[template.SheetNames[0]]
+
+        temp_sheet['!cols'] = [
+            { wch: 7 },
+            { wch: 35 },
+            { wch: 30 },
+            { wch: 30 },
+            { wch: 30 },
+            { wch: 30 },
+            { wch: 30 },
+            { wch: 7 },
+            { wch: 7 },
+            { wch: 7 }
+        ];
+        const entry_dist = 5;
+
+        let entry = 14;
+        const column_lista = ['C', 'D', 'E', 'F']
+        const column_listb = ['C', 'D', 'E', 'F', 'G']
+
+
+        let sortedStudents = await students.sort((a, b) => {
+            // Compare two elements based on a specific property
+            // For example, sorting based on the name property
+            if (a.pid < b.pid) {
+                return -1; // a should come before b in the sorted order
+            } else {
+                return 1; // a should come after b in the sorted order
+            }
+        });
+
+        await sortedStudents.forEach((student) => {
+            // console.log('hello')
+            temp_sheet[`A${entry}`] = { t: 's', v: student.pid };
+            column_lista.forEach((item, index) => {
+                //console.log('item', item, 'iter:', index)
+
+                if (index < SubList.length && student.practical && student.term && student.oral) {
+                    console.log(index, "hello here")
+                    if (student.practical.hasOwnProperty(SubList[index])) {
+                        temp_sheet[`${item}${entry}`] = { t: 's', v: (student.practical && student.practical[SubList[index]]) ?? 'no data' }
+                    }
+                    if (student.term.hasOwnProperty(SubList[index]) && student.oral.hasOwnProperty(SubList[index])) {
+                        temp_sheet[`${item}${entry + 1}`] = {
+                            t: 's', v: student.oral && student.term && student.oral[SubList[index]] && student.term[SubList[index]] ? `${student.term[SubList[index]]}/${student.oral[SubList[index]]}` : 'no data'
+                        }
+                    }
+                }
+            })
+
+            column_listb.forEach((item, index) => {
+                //console.log('item', item, 'iter:', index)
+                if (index + 5 < SubList.length && student.practical && student.term && student.oral) {
+                    if (student.practical.hasOwnProperty(SubList[index + 5])) {
+
+                        temp_sheet[`${item}${entry + 2} `] = { t: 's', v: student.practical && student.practical[SubList[index + 5]] ? student.practical[SubList[index + 5]] : 'no data' }
+                    }
+
+                    if (student.term.hasOwnProperty(SubList[index + 5]) && student.oral.hasOwnProperty(SubList[index + 5])) {
+                        temp_sheet[`${item}${entry + 3} `] = {
+                            t: 's', v: student.oral && student.term && student.oral[SubList[index + 5]] && student.term[SubList[index + 5]] ? `${student.term[SubList[index + 5]]}/${student.orals[SubList[index + 5]]}` : 'no data'
+                        }
+                    }
+                }
+            })
+            entry += entry_dist
+        })
+        const newbook = xlsx.utils.book_new();
+        temp_sheet['!ref'] = 'A1:J500'
+
+        xlsx.utils.book_append_sheet(newbook, temp_sheet, 'test1')
+
+        xlsx.writeFile(newbook, 'temp2.xlsx')
+
+        res.send({ book: newbook })
+
+    } catch (err) {
+        console.log(err)
+        res.send(err)
+
+
+    }
+})
+
+router.post('/get_exams', async (req, res) => {
+    try {
+        // Find teacher subjects based on the teacher_id from the request body
+        const teacherSubjects = await TeacherSubjectsSchema.find({ teacher_id: req.body.teacher_id })
+
+        // Initialize an empty array to store the result
+        let DisplayList = []
+
+        // Loop through each teacher subject
+        for (let item of teacherSubjects) {
+            // Populate the 'subject_id' field for each teacher subject
+            const populatedItem = await TeacherSubjectsSchema.findById(item._id).populate('subject_id')
+            filteredItem = {
+                subject_id: populatedItem.subject_id.subject_id,
+                semester: populatedItem.subject_id.subject_id.charAt(3),
+                class: populatedItem.class,
+                subject_name: populatedItem.subject_id.subject_name,
+                branch: populatedItem.subject_id.branch
+            }
+
+            console.log(filteredItem)
+            // Check if practical exam is created for this subject
+            if (populatedItem.created.practical === true) {
+                // If practical exam is created, add it to the display list
+                DisplayList.push({ ...filteredItem, marks_type: 'practical' })
+            }
+
+            if (populatedItem.created.oral === true) {
+                // If practical exam is created, add it to the display list
+                DisplayList.push({ ...filteredItem, marks_type: 'oral' })
+            }
+            if (populatedItem.created.term === true) {
+                // If practical exam is created, add it to the display list
+                DisplayList.push({ ...filteredItem, marks_type: 'term' })
+            }
+        }
+
+        // Send the display list as the response
+        res.json(DisplayList)
+
+    } catch (err) {
+        // Handle errors
+        console.error(err)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+
 
 
 module.exports = router;   
