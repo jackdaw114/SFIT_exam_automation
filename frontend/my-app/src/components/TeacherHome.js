dae028762420af6351393d8bba8da5e726bac932
jackdaw <jasonsampypvt@gmail.com/home/jackdaw/Documents/GitHub/opengl_windowsXlinux/build/bin/GL_project>
2024-04-22 16:26:48 +0530

    good boy

--- frontend/my-app/src/components/TeacherHome.js
+++ frontend/my-app/src/components/TeacherHome.js
@@ -30,11 +30,11 @@ export default function TeacherHome() {
             <div>
 
 
-                <div className="fixed h-full bg- flex flex-col bg-zinc-800 items-center  w-1/5 font-['ubuntu'] ">
+                <div className="fixed h-full  flex flex-col bg-zinc-800 items-center  w-1/5 font-['ubuntu'] " >
                     <Box className=" flex w-4/5 bg-inherit py-5  justify-center items-center" sx={{ borderBottom: 3, borderBottomColor: '#136F63' }} >
                         <Typography className=" font-['ubuntu'] text-white/90 " variant="h4">Navigation</Typography>
                     </Box>
-                    <div className="w-full h-full bg-inherit text-white/90 py-6">
+                    <div className="h-full bg-inherit text-white/90 py-6">
                         {list_items.map((item) => (
                             <div key={item} onClick={() => handleItemClick(item)} className="cursor-pointer hover:text-secondary transition-all duration-200 py-5 px-5 text-center">{item}</div>
                         ))}
@@ -42,8 +42,8 @@ export default function TeacherHome() {
                 </div>
 
                 <div className="flex h-full w-full">
-                    <div className="w-1/5 h-full"></div>
-                    <div className="render_pages h-full bg-slate-200 w-4/5  ">
+                    <div className=" h-full" style={{ marginLeft: '20vw' }} ></div>
+                    <div className="render_pages h-full bg-slate-200 " style={{ width: '100%' }}>
 
                         <div className="  ">
                             {pageComponent}
