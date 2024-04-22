4706b819fcf063bfba89ebee72d7aaf42e5a71a4
Nigel <95841502+LearnerOfProgramming@users.noreply.github.com>
2024-04-22 15:41:12 +0530

    Update SheetPortal.js

--- frontend/my-app/src/components/SheetPortal.js
+++ frontend/my-app/src/components/SheetPortal.js
@@ -7,14 +7,16 @@ import { read, utils, write, writeFile } from "xlsx";
 import { saveAs } from "file-saver";
 import ExcelCard from "./ExcelCard";
 import './Sheets.css'
-import { useNavigate } from "react-router";
+import { Routes, useNavigate } from "react-router";
 import * as Romanice from 'romanice';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
 
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import SheetView from "./SheetView";
 
 
 
@@ -34,15 +36,9 @@ export default function TeacherNav() {
     const navigate = useNavigate()
     const [class_name, setClassName] = useState([])
     const [subjectList, setSubjectList] = useState([])
-    const handleChangeType = (e) => {
-        console.log(e.target)
-        setType(e.target.value)
-    }
-    const handleChangeClass = (e) => {
-        console.log(e.target)
-        console.log(subjectList.find(item => item.subject_id === subject)?.class)
-        setClassName(e.target.value)
-    }
+
+
+
     const handleChangeSubject = (e) => {
         const selectedSubjectId = e.target.value;
         setSubject(e.target.value)
@@ -84,28 +80,17 @@ export default function TeacherNav() {
     }
         , [])
 
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     const handleSubmit = async (e) => {
         //  let jsonData = []
         const theJsonData = []
-        console.log(subjectId)
         console.log(subject.charAt(3))
         axios.post('/jason/create_student_marks', {
             semester: parseInt(subject.charAt(3)),
             marks_type: type,
             subject: subject,
             class: class_name,
-            teacher_id: localStorage.getItem('username'),
-            subject_id: subjectId
+            teacher_id: localStorage.getItem('username')
         }, {
 
             headers: {
@@ -137,69 +122,93 @@ export default function TeacherNav() {
         <>
             <div className='container-teacher-nav w-12/12'>
 
-                <Box className='selection-box'>
-                    <Box sx={{ borderRadius: 2, margin: 2, backgroundColor: "white", display: 'flex', justifyContent: 'space-around', boxSizing: "100%", padding: 5, alignItems: 'center' }}>
-                        <Box sx={{ display: 'flex', minWidth: '85%' }} className=" justify-evenly">
+                <Routes>
+                    <Route path="/" element={<Home subjectList={subjectList} handleChangeSubject={handleChangeSubject} type={type} setType={setType} subject={subject} setSubject={setSubject} class_name={class_name} setClassName={setClassName} handleSubmit={handleSubmit} cardData={cardData} />} />
+                    <Route path="/viewexam" element={<SheetView />} />
+                </Routes>
+
+            </div>
+        </>
+    )
+}
+
+function Home({ subjectList, handleChangeSubject, type, setType, subject, setSubject, class_name, setClassName, handleSubmit, cardData }) {
+
+    const handleChangeType = (e) => {
+        console.log(e.target)
+        setType(e.target.value)
+    }
+    const handleChangeClass = (e) => {
+        console.log(e.target)
+        console.log(subjectList.find(item => item.subject_id === subject)?.class)
+        setClassName(e.target.value)
+    }
+
+    return (
+        <>
+            <Box className='selection-box'>
+                <Box sx={{ borderRadius: 2, margin: 2, backgroundColor: "white", display: 'flex', justifyContent: 'space-around', boxSizing: "100%", padding: 5, alignItems: 'center' }}>
+                    <Box sx={{ display: 'flex', minWidth: '85%' }} className=" justify-evenly">
 
 
 
-                            <FormControl sx={{ minWidth: 200 }}>
-                                <InputLabel>Subject</InputLabel>
-                                <Select onChange={(e) => handleChangeSubject(e)} value={subject} label="Subject" >
-                                    {subjectList.map((item, index) => (
+                        <FormControl sx={{ minWidth: 200 }}>
+                            <InputLabel>Subject</InputLabel>
+                            <Select onChange={(e) => handleChangeSubject(e)} value={subject} label="Subject" >
+                                {subjectList.map((item, index) => (
 
-                                        <MenuItem value={item.subject_id} data-id={item._id}>{item.subject_id} - {item.subject_name}</MenuItem>
+                                    <MenuItem value={item.subject_id} data-id={item._id}>{item.subject_id} - {item.subject_name}</MenuItem>
 
-                                    ))}
+                                ))}
 
+                            </Select>
+                        </FormControl>
+                        <FormControl sx={{ minWidth: 200 }}>
+                            <InputLabel>Select Type of Marks</InputLabel>
+                            {subject ? (
+                                subjectList.map((item, index) => {
+
+                                    if (item.subject_id === subject) {
+                                        return (
+                                            <Select onChange={handleChangeType} value={type} label="Select Examination">
+
+                                                {item.practical && <MenuItem key={index} value='practical'>Practical</MenuItem>}
+                                                {item.oral && <MenuItem key={index} value='oral'>Oral</MenuItem>}
+                                                {item.term && <MenuItem key={index} value='term'>Term</MenuItem>}
+
+                                            </Select>
+                                        );
+                                    }
+                                    return null; // If the subject_id doesn't match, return null
+                                })
+                            ) : (
+                                <Select onChange={handleChangeType} value={type} label="Select Examination">
+                                    <MenuItem value=''>Please Select Subject</MenuItem>
                                 </Select>
-                            </FormControl>
-                            <FormControl sx={{ minWidth: 200 }}>
-                                <InputLabel>Select Type of Marks</InputLabel>
-                                {subject ? (
-                                    subjectList.map((item, index) => {
-
-                                        if (item.subject_id === subject) {
-                                            return (
-                                                <Select onChange={handleChangeType} value={type} label="Select Examination">
-
-                                                    {item.practical && <MenuItem key={index} value='practical'>Practical</MenuItem>}
-                                                    {item.oral && <MenuItem key={index} value='oral'>Oral</MenuItem>}
-                                                    {item.term && <MenuItem key={index} value='term'>Term</MenuItem>}
-
-                                                </Select>
-                                            );
-                                        }
-                                        return null; // If the subject_id doesn't match, return null
+                            )}
+                        </FormControl>
+                        <FormControl sx={{ minWidth: 200 }}>
+                            <InputLabel>Enter class</InputLabel>
+                            {subject ? (<>
+                                {console.log("MEOW", subjectList.find(item => item.subject_id === subject).class)}
+                                <Select onChange={handleChangeClass} value={class_name}>
+                                    {[subjectList.find(item => item.subject_id === subject).class].map((item, index) => {
+                                        return <MenuItem key={index} value={item} > {item} </MenuItem>
                                     })
-                                ) : (
-                                    <Select onChange={handleChangeType} value={type} label="Select Examination">
-                                        <MenuItem value=''>Please Select Subject</MenuItem>
-                                    </Select>
-                                )}
-                            </FormControl>
-                            <FormControl sx={{ minWidth: 200 }}>
-                                <InputLabel>Enter class</InputLabel>
-                                {subject ? (<>
-                                    {console.log("MEOW", subjectList.find(item => item.subject_id === subject).class)}
-                                    <Select onChange={handleChangeClass} value={class_name}>
-                                        {[subjectList.find(item => item.subject_id === subject).class].map((item, index) => {
-                                            return <MenuItem key={index} value={item} > {item} </MenuItem>
-                                        })
-                                        }
-                                    </Select>
-                                </>
-                                ) : (
-                                    <Select onChange={handleChangeClass} value={class_name} label="Select Examination">
-                                        <MenuItem value='' key={10} >Please Select Subject</MenuItem>
-                                    </Select>
-                                )}
-                            </FormControl>
-                        </Box>
-                        <div>
+                                    }
+                                </Select>
+                            </>
+                            ) : (
+                                <Select onChange={handleChangeClass} value={class_name} label="Select Examination">
+                                    <MenuItem value='' key={10} >Please Select Subject</MenuItem>
+                                </Select>
+                            )}
+                        </FormControl>
+                    </Box>
+                    <div>
 
-                            <Button variant="contained" color="warning" onClick={handleSubmit}>Open</Button>
-                            {/* <Dialog
+                        <Button variant="contained" color="warning" onClick={handleSubmit}>Open</Button>
+                        {/* <Dialog
                                 open={open}
                                 onClose={handleClose}
                                 aria-labelledby="alert-dialog-title"
@@ -222,28 +231,29 @@ export default function TeacherNav() {
                                 </DialogActions>
                             </Dialog> */}
 
-                        </div>
-                    </Box>
+                    </div>
                 </Box>
+            </Box>
 
-                <div className="pb-20 ">
+            <div className="pb-20 ">
 
-                    <Grid container spacing={1} className=" flex justify-center">
-
-
-                        {cardData.map((val, index) => (
-                            <Grid item xs={12}>
-                                <ExcelCard marks_type={val.marks_type} subject={[val.subject_id, val.subject_name]} semester={val.semester} department={val.branch} class={val.class} ></ExcelCard>
-
-                            </Grid>
-
-                        ))}
+                <Grid container spacing={1} className=" flex justify-center">
 
 
-                    </Grid>
-                </div>
+                    {cardData.map((val, index) => (
+                        <Grid item xs={12}>
+                            <ExcelCard marks_type={val.marks_type} subject={[val.subject_id, val.subject_name]} semester={val.semester} department={val.branch} class={val.class} ></ExcelCard>
 
-            </div >
+                        </Grid>
+
+                    ))}
+
+
+                </Grid>
+            </div>
+
+
+
         </>
-    )
+    );
 }
