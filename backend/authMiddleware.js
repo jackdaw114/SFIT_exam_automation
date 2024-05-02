7ed3e9be6ed9c8c4424beeebca541529ad8d39fb
jackdaw <jasonsampypvt@gmail.com/home/jackdaw/Documents/GitHub/opengl_windowsXlinux/build/bin/GL_project>
2024-05-02 16:08:28 +0530

    weee

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -6,7 +6,7 @@ function authAdmin(req, res, next) {
     const jwtToken = req.cookies.jwtToken;
     if (jwtToken == null) return res.sendStatus(401);
 
-    jwt.verify(jwtToken, process.env.TEACHER_SECRET_KEY, (err, decoded) => {
+    jwt.verify(jwtToken, process.env.ADMIN_SECRET_KEY, (err, decoded) => {
         if (err) return res.sendStatus(403); // Forbidden
         req.user = decoded;
         console.log(decoded);
@@ -20,7 +20,7 @@ function authTeacher(req, res, next) {
     const jwtToken = req.cookies.jwtToken;
     if (jwtToken == null) return res.sendStatus(401);
 
-    jwt.verify(jwtToken, process.env.ADMIN_SECRET_KEY, (err, decoded) => {
+    jwt.verify(jwtToken, process.env.TEACHER_SECRET_KEY, (err, decoded) => {
         if (err) return res.sendStatus(403); // Forbidden
         req.user = decoded;
         console.log(decoded);
