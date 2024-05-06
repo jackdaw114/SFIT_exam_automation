916cfc791f871ca35eb6d2bfb55a03e391a5c2f6
jackdaw <jasonsampypvt@gmail.com/home/jackdaw/Documents/GitHub/opengl_windowsXlinux/build/bin/GL_project>
2024-05-06 10:28:39 +0530

    length

--- frontend/my-app/src/components/NotifButton.js
+++ frontend/my-app/src/components/NotifButton.js
@@ -24,7 +24,7 @@ export function NotifButton({ }) {
         <Button variant="outlined" className=" text-teal-400 font-['Ubuntu'] border-none" >
             Subscription Requests
             {
-                data ?
+                data.length ?
                     <span class=" ml-2 relative flex h-3 w-3 ">
                         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-yellow-500 opacity-75"></span>
                         <span class="relative inline-flex rounded-full h-3 w-3 bg-yellow-500"></span>
